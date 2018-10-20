116b24099e8abd09071c5da538107594b7b01f2f
Arnaud Buathier <arnaud@arnapou.net>
2018-10-20 09:28:24 +0200

    exemple de fichier de config

---
+++ config.dist.php
@@ -0,0 +1,24 @@
+<?php
+
+define('GW2RAIDAR_URL', 'https://www.gw2raidar.com/');
+define('GW2RAIDAR_USER', '<username>');
+define('GW2RAIDAR_PASS', '<password>');
+
+define('DPSREPORT_URL', 'https://dps.report/');
+define('DPSREPORT_TOKEN', '<token>');
+
+define('ACCOUNTS', [
+    'Account.1234'     => '<api_key>',
+]);
+
+define('PROCESS_MAX_EXECUTION_TIME', 900);
+define('PROCESS_INTERVAL_INCREMENT', 300);
+define('PROCESS_INTERVAL_MAXIMUM', 3600);
+define('PROCESS_TTL_BEFORE_DISABLED', 7 * 86400);
+
+define('UPLOAD_PARAMETER_NAME', '<random_string_to_generate>');
+
+define('MENU', [
+    '/index.php'    => 'Logs',
+    '/progress.php' => 'Progress',
+]);
